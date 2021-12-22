c3842c3c76edf845d04c2610723a5f75b7154155
Dee-Hegde <dee.sirsi@gmail.com>
2021-12-22 23:22:51 +0530

    bugs fixed

--- simpletodo/src/Components/TodoItem.jsx
+++ simpletodo/src/Components/TodoItem.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from "./Todo.module.css"
 
-function TodoItem({item,handleStatus,handleDelete}) {
+function TodoItem({item,handleStatus,handleDelete,handleTodo}) {
     return (
         <div key={item.id}>
             <h2>{item.title}</h2>
-            <p>{item.status?"Completed":"InComplete"}</p>
-            <button onClick={()=>handleStatus(item.id)}>Status</button>
-            <button onClick={()=>handleDelete(item.id)}>Delete</button>
+            <p>{(item.complete?"Completed":(!item.status?"Todo ":"Inprogress"))}</p>
+            <button className={styles.button} disabled={item.complete} onClick={()=>handleStatus(item.id)}>Status</button>
+            <button className={styles.button} onClick={()=>handleTodo(item.id)}>{!item.complete ? "Mark Completed":"Mark Incomplete"}</button>
+            <button className={styles.button} onClick={()=>handleDelete(item.id)}>Delete</button>
         </div>
     )
 }
--- simpletodo/src/Components/TodoList.jsx
+++ simpletodo/src/Components/TodoList.jsx
@@ -3,16 +3,14 @@ import styles from "./Todo.module.css"
 import TodoItem from './TodoItem'
 
 function TodoList({todo,setTodo}) {
-   const todo1=todo.filter(item=>!item.status && !item.todo);
-   const incomplete= todo.filter(item=>!item.todo && item.status)
-   const completed1= todo.filter(item=>item.todo && item.status)
+   const todo1=todo.filter(item=>!item.status && !item.complete);
+   const incomplete= todo.filter(item=>!item.complete && item.status)
+   const completed1= todo.filter(item=>item.complete)
 
-   // const filterFunction=()=>{
-
-   // }
-   // useEffect(() => {
-   //    filterFunction()
-   // }, [])
+   const handleTodo=(id)=>{
+      const updatedtodo=todo.map(item=>id===item.id? {...item, complete:!item.complete, status:!item.status }:item)
+      setTodo(updatedtodo)
+   }
     const handleStatus=(id)=>{
         const change=todo.map(item=>id===item.id? {...item, status:!item.status}:item)
         setTodo(change)
@@ -21,6 +19,7 @@ function TodoList({todo,setTodo}) {
          const deleted =todo.filter(item=> item.id !== id)
          setTodo(deleted)
      }
+
     
     return (
         <div >
@@ -28,15 +27,27 @@ function TodoList({todo,setTodo}) {
           <div className={styles.flexdiv}>
                <div className={styles.marginleft}>
                <h2>Todo List:</h2>
-                  {todo1.map(item=><TodoItem item={item} handleDelete={handleDelete} handleStatus={handleStatus}/>)}
+                  {todo1.map(item=><TodoItem 
+                  item={item}
+                   handleDelete={handleDelete}
+                   handleTodo={handleTodo} 
+                   handleStatus={handleStatus}/>)}
                </div>
                <div className={styles.marginleft}>
                <h2>In Progress:</h2>
-                  {incomplete.map(item=><TodoItem item={item} handleDelete={handleDelete} handleStatus={handleStatus}/>)}
+                  {incomplete.map(item=><TodoItem 
+                  item={item}
+                  handleDelete={handleDelete}
+                  handleTodo={handleTodo}
+                  handleStatus={handleStatus}/>)}
                </div>
                <div className={styles.marginleft}>
                <h2>Completed:</h2>
-                  {completed1.map(item=><TodoItem item={item} handleDelete={handleDelete} handleStatus={handleStatus}/>)}
+                  {completed1.map(item=><TodoItem 
+                  item={item} 
+                  handleDelete={handleDelete} 
+                  handleTodo={handleTodo}
+                   handleStatus={handleStatus}/>)}
                </div>
           </div>
         </div>
